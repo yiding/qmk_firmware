66c7794dc9ad7f893b3d39a1550dee815b0fbffa
Yiding Jia <yiding.jia@gmail.com>
2018-01-09 23:55:17 -0800

    Add insert and delete key to layer 1

--- keyboards/ergodox_ez/keymaps/yiding/keymap.c
+++ keyboards/ergodox_ez/keymaps/yiding/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                         KC_TRNS, KC_TRNS,
                                                  KC_TRNS,
-                               KC_TRNS, KC_TRNS, KC_TRNS,
+                                KC_DEL, KC_INS,  KC_TRNS,
        // right hand
        KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_TRNS, KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F12,
